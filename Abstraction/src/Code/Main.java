22ba6a2cd7ed8d3cbfa5f1a671e45b3fd1b1ec21
Arman Hossen <2armanhossen@gmail.com>
2025-07-27 01:42:57 +0600

    Farhan

---
+++ Abstraction/src/Code/Main.java
@@ -0,0 +1,14 @@
+package Code;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		// abstract = abstract classes cannot be instantiated, but they can have a subclass
+		//            abstract methods are declared without an implementation,, 
+		/// we can't  create the object of this abstract class but we can inherit or them and use their methods 
+
+		
+		
+		
+	}
+}
