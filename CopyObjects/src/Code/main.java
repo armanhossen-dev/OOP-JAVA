de0850f0a48bbbfbe71243d52c2108dfd25554c5
Arman Hossen <2armanhossen@gmail.com>
2025-07-28 04:45:01 +0600

    oop

---
+++ CopyObjects/src/Code/main.java
@@ -0,0 +1,11 @@
+package Code;
+
+public class main {
+
+	public static void main(String[] args) {
+		// playlist : https://www.youtube.com/watch?v=ljGH04_ASG4&list=PLxuuH5GnCIlcGnesYMkGQOqokyI2Fwu3g&index=17
+		// video: + https://youtu.be/ljGH04_ASG4?list=PLxuuH5GnCIlcGnesYMkGQOqokyI2Fwu3g
+
+	}
+
+}
